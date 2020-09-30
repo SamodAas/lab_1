997aea5d690f632558cfd4d21f91732786277123
Adomas Nasutavicius <adomas.nasutavicius@knf.stud.vu.lt>
2020-09-30 12:03:37 +0300

    Added change button Added two text view elements which change uppon pressing the button

--- app/src/main/java/com/example/lab1/MainActivity.java
+++ app/src/main/java/com/example/lab1/MainActivity.java
@@ -3,12 +3,35 @@ package com.example.lab1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView tvleft;
+    private TextView tvright;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+
+        this.tvleft = findViewById(R.id.textleft);
+        this.tvright = findViewById(R.id.textright);
+            }
+
+
+    public void onButtonClick(View view) {
+        String left = this.tvleft.getText().toString();
+        String right = this.tvright.getText().toString();
+        String thestring = "Pakeista";
+        if(left.equals(thestring)){
+            this.tvleft.setText("Nepakeista");
+        } else {
+            this.tvleft.setText("Pakeista");
+        }
+        if(right.equals(thestring)){
+            this.tvright.setText("Nepakeista");
+        } else {
+            this.tvright.setText("Pakeista");
+        }}
 }
